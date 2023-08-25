6e797e6430e4b86d4064e949b49abe82755feed7
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-08-25 18:46:10 +0700

    add delete feature

--- todo_list/src/components/Taskwrapper.js
+++ todo_list/src/components/Taskwrapper.js
@@ -1,20 +1,62 @@
 import React, { useState } from "react";
 import TaskForm from "./TaskForm";
+import Task from "./Task";
+
 
 function Taskwrapper() {
-    const [todos, setTodos] = useState([]);
-    const addForm = (todo) => {
-        setTodos([
-          ...todos,
-          { id: Math.round(Math.random()*9999), task: todo, completed: false, isEditing: false },
-        ]);
-      }
-    return (
-        <div className="TaskWrapper">
-            <h1>TO DO APP</h1>
-            <TaskForm  addForm={addForm}/>
-        </div>
-  )
+  const [todos, setTodos] = useState([]);
+  const addForm = (todo) => {
+    setTodos([
+      ...todos,
+      {
+        id: Math.round(Math.random() * 9999),
+        task: todo,
+        completed: false,
+        isEditing: false,
+      },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+
+
+  return (
+    <div className="TaskWrapper">
+      <h1>TO DO APP</h1>
+      <TaskForm addForm={addForm} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+        
+            console.log('hello')
+        ) : (
+          <Task
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
 }
 
 export default Taskwrapper;
